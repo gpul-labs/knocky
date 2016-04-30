e9d25b1e09aece1b9496b3a433e738988547b8b3
Alberto <amtaboada99@gmail.com>
2016-04-30 14:13:54 +0200

    Initial version of the Arduino code

--- knocky_code/knocky_code.ino
+++ knocky_code/knocky_code.ino
@@ -1,8 +1,23 @@
+/* Copyright {2016} {Rach95}
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 int pinPiezo = A0;
 int oldValue = 10;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(pinPiezo, INPUT);
 }
 
@@ -26,8 +41,8 @@ void loop() {
     } else if (newValue < oldValue) {
       maxValue = oldValue;
 
-      Serial.println("Valor maximo: " + oldValue);
-      Serial.println("Duracion: " + duration);
+      Serial.println("Valor maximo: " + (String) oldValue);
+      Serial.println("Duracion: " + (String) duration);
       duration = 0;
     }
   }
